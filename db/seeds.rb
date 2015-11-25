4ef91083128d5314a757b273d027544c7fd36762
Laevo <rohitvgaikwad@mavs.uta.edu>
2015-11-25 16:41:04 -0600

    First

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# heroku run rake db:seed --app cse-rvg6285-2
+require "redis"
+# require 'mongo'
+
+
+
+# Inserting into Redis
+redis = Redis.new
+redisfile = File.read('redismongo.json')
+redis_data = JSON.parse(redisfile)
+redis_data.each do |key, value|
+   redis.set("#{key}", "#{value}")  
+  end
+
+# # Inserting into mongo
+# mongo_uri = ENV['MONGOLAB_URI']
+# client = Mongo::Client.new(mongo_uri);
+# db = client.database
+# mongofile = File.read('redismongo.json')
+# mongo_data = JSON.parse(mongofile)
+#   mongo_data.each do |key, value|
+#       db[:population].insert_one({_id: "#{key}", population:"#{value}"})
+#  end
